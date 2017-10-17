e37b6874fd5e699e20e2625db8d13a94338b4006
Ray <zsirui@126.com>
2017-10-17 10:27:53 +0800

    Update lcd.h

--- Inc/lcd.h
+++ Inc/lcd.h
@@ -1,41 +1,51 @@
 /**
-	******************************************************************************
-	* File Name          : LCD.h
-	* Description        : This file provides code for the configuration
-	*                      of the LCD instances.
-	******************************************************************************
-	** This notice applies to any and all portions of this file
-	* that are not between comment pairs USER CODE BEGIN and
-	* USER CODE END. Other portions of this file, whether 
-	* inserted by the user or by software development tools
-	* are owned by their respective copyright owners.
-	*
-	* COPYRIGHT(c) 2017 STMicroelectronics
-	*
-	* Redistribution and use in source and binary forms, with or without modification,
-	* are permitted provided that the following conditions are met:
-	*   1. Redistributions of source code must retain the above copyright notice,
-	*      this list of conditions and the following disclaimer.
-	*   2. Redistributions in binary form must reproduce the above copyright notice,
-	*      this list of conditions and the following disclaimer in the documentation
-	*      and/or other materials provided with the distribution.
-	*   3. Neither the name of STMicroelectronics nor the names of its contributors
-	*      may be used to endorse or promote products derived from this software
-	*      without specific prior written permission.
-	*
-	* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-	* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-	* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-	* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-	* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-	* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	*
-	******************************************************************************
-	*/
+  ******************************************************************************
+  * File Name          : LCD.h
+  * Description        : This file provides code for the configuration
+  *                      of the LCD instances.
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __lcd_H
 #define __lcd_H
@@ -56,9 +66,24 @@ extern LCD_HandleTypeDef hlcd;
 /* USER CODE BEGIN Private defines */
 typedef enum
 {
-	POINT_OFF = 0,
-	POINT_ON = 1
+    POINT_OFF = 0,
+    POINT_ON = 1
 }Point_Typedef;
+
+typedef enum
+{
+    Mode_0 = 0,
+	Mode_1,
+	Mode_2,
+	Mode_3,
+	Mode_Err = -1
+}Mode_Typedef;
+
+typedef enum
+{
+    MINUS_OFF = 0,
+    MINUS_ON = 1
+}Minus_Typedef;
 
 #define MCU_LCD_COM0          LCD_RAM_REGISTER0
 #define MCU_LCD_COM1          LCD_RAM_REGISTER2
@@ -106,14 +131,14 @@ typedef enum
 #define LCD_SEG6          MCU_LCD_SEG6
 #define LCD_SEG7          MCU_LCD_SEG7
 
-#define LCD_DIGIT_COM0				LCD_COM0
-#define LCD_DIGIT_COM0_SEG_MASK		~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
-#define LCD_DIGIT_COM1				LCD_COM1
-#define LCD_DIGIT_COM1_SEG_MASK		~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
-#define LCD_DIGIT_COM2				LCD_COM2
-#define LCD_DIGIT_COM2_SEG_MASK		~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
-#define LCD_DIGIT_COM3				LCD_COM3
-#define LCD_DIGIT_COM3_SEG_MASK		~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
+#define LCD_DIGIT_COM0              LCD_COM0
+#define LCD_DIGIT_COM0_SEG_MASK     ~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
+#define LCD_DIGIT_COM1              LCD_COM1
+#define LCD_DIGIT_COM1_SEG_MASK     ~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
+#define LCD_DIGIT_COM2              LCD_COM2
+#define LCD_DIGIT_COM2_SEG_MASK     ~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
+#define LCD_DIGIT_COM3              LCD_COM3
+#define LCD_DIGIT_COM3_SEG_MASK     ~(LCD_SEG0 | LCD_SEG1 | LCD_SEG2 | LCD_SEG3 | LCD_SEG4 | LCD_SEG5 | LCD_SEG6 | LCD_SEG7)
 
 #define LCD_SEG0_SHIFT          MCU_LCD_SEG0_SHIFT
 #define LCD_SEG1_SHIFT          MCU_LCD_SEG1_SHIFT
@@ -127,7 +152,7 @@ typedef enum
 
 extern void _Error_Handler(char *, int);
 void MX_LCD_Init(void);
-void LCD_GLASS_DisplayFloat(float src);
+void LCD_GLASS_Heartbeat(float src);
 void LCD_GLASS_Clear(void);
 void LCD_GLASS_BlinkConfig(void);
 
@@ -141,11 +166,11 @@ void LCD_GLASS_BlinkConfig(void);
 #endif /*__ lcd_H */
 
 /**
-	* @}
-	*/
+  * @}
+  */
 
 /**
-	* @}
-	*/
+  * @}
+  */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
